230624cacc51bbfad26d822c77f1020254cb2be5
Karel Burda <karelburda89@gmail.com>
2022-08-11 17:10:36 +0200

    Updated implementation that uses iterators and pairs

--- include/constexpr_hash_map/constexpr_hash_map.hpp
+++ include/constexpr_hash_map/constexpr_hash_map.hpp
@@ -1,9 +1,9 @@
-#pragma once
+#ifndef BURDA_CONSTEXPR_HASH_MAP_HPP
+#define BURDA_CONSTEXPR_HASH_MAP_HPP
 
 #include <array>
 #include <cstddef>
 #include <iterator>
-#include <stdexcept>
 #include <utility>
 
 namespace burda::ct
@@ -12,27 +12,43 @@ template <std::size_t N, typename K, typename V>
 class hash_map
 {
 public:
+    using key_type = K;
+    using value_type = V;
+    using size_type = decltype(N);
+    using data_type = std::array<std::pair<K, V>, N>;
+    using const_iterator = typename data_type::const_iterator;
+
     template<typename... E>
-    explicit constexpr hash_map(E&&... elements)
+    explicit constexpr hash_map(E&&... elements) noexcept
     : data{std::forward<E>(elements)...}
     {
         static_assert(N > 0, "N should be positive");
         static_assert(N == sizeof...(elements), "Elements size doesn't match expected size of a hash-map");
     }
 
-    [[nodiscard]] constexpr const V& at(const K& key) const
+    [[nodiscard]] constexpr const_iterator find(const K& key) const noexcept
     {
-        const auto it = search<0, N>(key);
-
-        if (it != std::cend(data))
-            return it->second;
-
-        throw std::out_of_range{"Element not found"};
+        return search<0, N>(key);
     }
 
-    [[nodiscard]] constexpr const V& operator[](const K& key) const
+    // deliberately not throwing an exception, and returning pair instead,
+    // as this generates much shorter assembly on clang and msvc
+    [[nodiscard]] constexpr std::pair<bool, const V&> at(const K& key) const noexcept
     {
-        return at(key);
+        const auto it = find(key);
+
+        if (it != cend())
+        {
+            return {true, it->second};
+        }
+
+        return {false, {}};
+    }
+
+    // doesn't do any bounds checking
+    [[nodiscard]] constexpr const V& operator[](const K& key) const noexcept
+    {
+        return find(key)->second;
     }
 
     [[nodiscard]] constexpr bool contains(const K& key) const noexcept
@@ -40,20 +56,47 @@ public:
         return search<0, N>(key) != std::cend(data);
     }
 
-    [[nodiscard]] constexpr std::size_t size() const noexcept
+    [[nodiscard]] constexpr size_type size() const noexcept
     {
         return data.size();
     }
 
-private:
-    using index_t = std::size_t;
+    // need for for-each cycle
+    [[nodiscard]] constexpr const_iterator begin() const noexcept
+    {
+        return cbegin();
+    }
 
-    template <index_t L, index_t R>
-    [[nodiscard]] constexpr auto search(const K& key) const
+    [[nodiscard]] constexpr const_iterator cbegin() const noexcept
+    {
+        return std::cbegin(data);
+    }
+
+    // need for for-each cycle
+    [[nodiscard]] constexpr const_iterator end() const noexcept
+    {
+        return cend();
+    }
+
+    [[nodiscard]] constexpr const_iterator cend() const noexcept
+    {
+        return std::cend(data);
+    }
+
+    [[nodiscard]] constexpr bool empty() const noexcept
+    {
+        return false;
+    }
+
+protected:
+    using index_type = size_type;
+
+    template <index_type L, index_type R>
+    [[nodiscard]] constexpr const_iterator search(const K& key) const noexcept
     {
         if constexpr (L < R)
         {
-            if (data[L].first == key)
+            if (equal(data[L].first, key))
             {
                 return std::next(std::cbegin(data), L);
             }
@@ -64,6 +107,21 @@ private:
         return std::cend(data);
     }
 
-    std::array<std::pair<K, V>, N> data;
+    template <typename T = K>
+    [[nodiscard]] constexpr bool equal(const T& a, const T& b) const noexcept
+    {
+        return a == b;
+    }
+
+    // specific implementation for const char* is needed
+    [[nodiscard]] constexpr bool equal(char const * a, char const * b) const noexcept
+    {
+        return *a == *b && (*a == '\0' || equal(a + 1, b + 1));
+    }
+
+private:
+    data_type data;
 };
 }
+
+#endif // BURDA_CONSTEXPR_HASH_MAP_HPP
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
+#include <string_view>
+
 #include <constexpr_hash_map/constexpr_hash_map.hpp>
 
-int main(int, const char**)
+int example_simple() noexcept
 {
-    constexpr const burda::ct::hash_map<2, const char*, int> map
+    static constexpr burda::ct::hash_map<2, const char*, int> map
     {
         std::make_pair("key1", 1),
         std::make_pair("key2", 2)
@@ -10,7 +12,7 @@ int main(int, const char**)
 
     static_assert(map.size() == 2);
     static_assert(map.contains("key1"));
-    static_assert(map.at("key1") == 1);
+    static_assert(map.at("key1").second == 1);
     static_assert(map.contains("key2"));
     static_assert(map["key2"] == 2);
     static_assert(!map.contains("key3"));
@@ -18,5 +20,36 @@ int main(int, const char**)
     //static_assert(map["key3"] == 3);
 
     // just as an example to observe generated assembly
-    return map.at("key2");
+    return map.at("key2").second;
+}
+
+int example_advanced() noexcept
+{
+    static constexpr burda::ct::hash_map<3, std::string_view, std::string_view> map
+    {
+        std::make_pair("key1", "value1"),
+        std::make_pair("key2", "value2"),
+        std::make_pair("key3", "value3")
+    };
+
+    // container supports iterators in a basic way
+    static constexpr auto it = map.find("key2");
+    static_assert(it != std::cend(map));
+    static_assert(it->second == "value2");
+    // another example calls
+    static_assert(!std::empty(map));
+    static_assert(std::size(map) == 3);
+
+    for ([[maybe_unused]] const auto& [key, value] : map)
+    {
+        // do something with the elements
+    }
+
+    // dummy example to observe constexpr nature of the generated assembly
+    return it->second.size();
+}
+
+int main(const int, const char**)
+{
+    return example_simple() + example_advanced();
 }
